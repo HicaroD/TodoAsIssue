b27743c8e0883e41f3409ec6d47e82adb0f8962a
HicaroD <hdanrlley1@gmail.com>
2022-08-28 17:41:38 -0300

    lexer: tokenizing characters

--- lexer.go
+++ lexer.go
@@ -1,35 +1,55 @@
 package main
 
-import "fmt"
+import (
+    "unicode"
+    "fmt"
+)
 
 type TokenKind int
 
 const (
-    Word TokenKind = iota
+    IssueBody TokenKind = iota
     Hash
-    DoubleQuotes
     OpeningPar
     ClosingPar
-    Number
+    IssueNumber
     Colon
 )
 
-type Token struct {
-    lexeme         string
-    tokenKind	   TokenKind
+func (token TokenKind) String() string {
+    switch token {
+    case IssueBody:
+	return "IssueBody"
+    case Hash:
+	return "Hash"
+    case OpeningPar:
+	return "OpeningPar"
+    case ClosingPar:
+	return "ClosingPar"
+    case IssueNumber:
+	return "Number"
+    case Colon:
+	return "Colon"
+    }
+    return "unknown"
 }
 
-func newToken(lexeme string, tokenKind TokenKind) Token {
+type Token struct {
+    lexeme  string
+    kind    TokenKind
+}
+
+func newToken(lexeme string, kind TokenKind) Token {
     return Token {
 	lexeme: lexeme,
-	tokenKind: tokenKind,
+	kind: kind,
     }
 }
 
 type Lexer struct {
-    fileContent string
-    cursor uint64
-    current_char byte
+    fileContent	   string
+    cursor	   uint64
+    current_char   rune
     is_end_of_file bool
 }
 
@@ -41,10 +61,48 @@ func NewLexer(content string) *Lexer {
     }
 }
 
+func (lexer *Lexer) get_issue_body() string {
+    lexer.advance_cursor()
+
+    var issue_body string
+    for lexer.current_char != '"' && !lexer.is_end_of_file {
+	issue_body += string(lexer.current_char)
+	lexer.advance_cursor()
+    }
+    return issue_body
+}
+
+func (lexer *Lexer) get_issue_number() string {
+    var number string
+    for unicode.IsDigit(lexer.current_char) && !lexer.is_end_of_file {
+	fmt.Printf("current char: %c\n", lexer.current_char)
+	number += string(lexer.current_char)
+	lexer.advance_cursor()
+    }
+    return number;
+}
+
 func (lexer *Lexer) tokenize() ([]Token, error) {
     var tokens []Token
     for !lexer.is_end_of_file {
-	fmt.Println(string(lexer.current_char))
+	switch(lexer.current_char) {
+	case '#':
+	    tokens = append(tokens, newToken(string(lexer.current_char), Hash))
+	case ':':
+	    tokens = append(tokens, newToken(string(lexer.current_char), Colon))
+	case '(':
+	    tokens = append(tokens, newToken(string(lexer.current_char), OpeningPar))
+	case ')':
+	    tokens = append(tokens, newToken(string(lexer.current_char), ClosingPar))
+	case '"':
+	    issue_body := lexer.get_issue_body()
+	    tokens = append(tokens, newToken(issue_body, IssueBody))
+	default:
+	    if unicode.IsDigit(lexer.current_char) {
+		number := lexer.get_issue_number()
+		tokens = append(tokens, newToken(number, IssueNumber))
+	    }
+	}
 	lexer.advance_cursor()
     }
     return tokens, nil
@@ -53,8 +111,8 @@ func (lexer *Lexer) tokenize() ([]Token, error) {
 func (lexer *Lexer) advance_cursor() {
     content_length := uint64(len(lexer.fileContent)) 
     if lexer.cursor < content_length - 1 {
+	lexer.current_char = rune(lexer.fileContent[lexer.cursor])
 	lexer.cursor++
-	lexer.current_char = lexer.fileContent[lexer.cursor]
     } else {
 	lexer.is_end_of_file = true
     }
