718de6b81dd57496efa6b1701cd981ae56bfd5ea
HicaroD <hdanrlley1@gmail.com>
2022-08-28 16:43:14 -0300

    main: reading file and passing to lexer

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "log"
+    "strings"
+)
 
 func main() {
-    fmt.Println("Hello, world!");
+    file, err := os.ReadFile("examples/todo.txt")
+    if err != nil {
+	log.Fatal(err)
+    }
+    content := string(file)
+    if strings.TrimSpace(content) == "" {
+	log.Fatal(fmt.Errorf("File is empty"));
+    }
+
+    lexer := NewLexer(content)
+    tokens, err := lexer.tokenize()
+    if err != nil {
+	log.Fatal(err)
+    }
+
+    for _, token := range(tokens) {
+	fmt.Println(token.lexeme)
+    }
 }
