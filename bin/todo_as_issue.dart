437fa739741288070c51b0f38276167551c7b0ef
HicaroD <hdanrlley1@gmail.com>
2022-11-04 08:54:45 -0300

    proj: removendo código desnecessário

--- bin/todo_as_issue.dart
+++ bin/todo_as_issue.dart
@@ -1,5 +1,3 @@
-import 'package:todo_as_issue/todo_as_issue.dart' as todo_as_issue;
-
 void main(List<String> arguments) {
-  print('Hello world: ${todo_as_issue.calculate()}!');
+  print("Hello, world!");
 }
