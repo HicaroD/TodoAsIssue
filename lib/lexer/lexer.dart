21d8422364c274a057ffca053a8f998701bde5f7
HicaroD <hdanrlley1@gmail.com>
2022-11-05 15:04:27 -0300

    lexer: implementando cursor para atravessar arquivo

--- lib/lexer/lexer.dart
+++ lib/lexer/lexer.dart
@@ -1,13 +1,28 @@
 import 'tokens.dart';
 
 class Lexer {
-  final String file_content;
+  final String fileContent;
+  int cursor = 0;
+  bool isEndOfFile = false;
+  late String current_character;
 
-  Lexer(this.file_content);
+  Lexer(this.fileContent);
+
+  void advance_cursor() {
+    if (cursor < fileContent.length - 1) {
+      current_character = fileContent[cursor];
+      cursor++;
+    } else {
+      isEndOfFile = true;
+    }
+  }
 
   List<Token> tokenize() {
     List<Token> tokens = [];
-
+    while (!isEndOfFile) {
+      advance_cursor();
+      print(current_character);
+    }
     return tokens;
   }
 }
