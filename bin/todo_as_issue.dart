a1817eb2987a1971a14d5d1dca46302265c36c0d
HicaroD <hdanrlley1@gmail.com>
2022-11-05 14:41:11 -0300

    main: lendo `todo.txt` e passando para o lexer

--- bin/todo_as_issue.dart
+++ bin/todo_as_issue.dart
@@ -1,3 +1,8 @@
-void main(List<String> arguments) {
-  print("Hello, world!");
+import 'dart:io';
+import 'package:todo_as_issue/lexer/lexer.dart';
+
+void main(List<String> args) async {
+  File file = File('examples/todo.txt');
+  String content = await file.readAsString();
+  Lexer lexer = Lexer(content);
 }
