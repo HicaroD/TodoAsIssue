48d482002f29336544b13d25a5c105c3cd08d0b8
HicaroD <hdanrlley1@gmail.com>
2022-11-05 14:37:40 -0300

    backend(lexer): adicionando lexer (não implementado)

---
+++ lib/lexer/lexer.dart
@@ -0,0 +1,13 @@
+import 'tokens.dart';
+
+class Lexer {
+  final String file_content;
+
+  Lexer(this.file_content);
+
+  List<Token> tokenize() {
+    List<Token> tokens = [];
+
+    return tokens;
+  }
+}
