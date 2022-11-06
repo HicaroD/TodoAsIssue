f6047f7d997fc2e6bd47c3170a60778f06dcc078
HicaroD <hdanrlley1@gmail.com>
2022-11-06 09:13:50 -0300

    main: chamando parser

--- bin/todo_as_issue.dart
+++ bin/todo_as_issue.dart
@@ -1,14 +1,16 @@
 import 'dart:io';
 import 'package:todo_as_issue/lexer/lexer.dart';
 import 'package:todo_as_issue/lexer/tokens.dart';
+import 'package:todo_as_issue/parser/parser.dart';
+import 'package:todo_as_issue/parser/todo.dart';
 
 void main(List<String> args) async {
   File file = File('examples/todo.txt');
   String content = await file.readAsString();
+
   Lexer lexer = Lexer(content);
   List<Token> tokens = lexer.tokenize();
 
-  for (Token token in tokens) {
-    print(token);
-  }
+  Parser parser = Parser(tokens);
+  List<Todo> todos = parser.parse();
 }
