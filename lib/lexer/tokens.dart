0f32a3fe762625a239418abe465068fd666fda98
HicaroD <hdanrlley1@gmail.com>
2022-11-06 09:01:10 -0300

    lexer: criando enum para representar os tipos de tokens

--- lib/lexer/tokens.dart
+++ lib/lexer/tokens.dart
@@ -1,6 +1,15 @@
 enum TokenKind {
   openingSquareBracket,
   closingSquareBracket,
+  hashSymbol,
+  openingParenthesis,
+  closingParenthesis,
+  colon,
+  quote,
+  issueName,
+  tilde,
+  number,
+  semicolon,
 }
 
 class Token {
@@ -8,4 +17,9 @@ class Token {
   final String lexeme;
 
   Token(this.kind, this.lexeme);
+
+  @override
+  String toString() {
+    return "$kind('$lexeme')";
+  }
 }
