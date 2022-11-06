13b27d616cd640da2b66b4f5cd951a38f40a569d
HicaroD <hdanrlley1@gmail.com>
2022-11-06 09:02:13 -0300

    lexer: chamando `lexer.tokenize()`

--- bin/todo_as_issue.dart
+++ bin/todo_as_issue.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
 import 'package:todo_as_issue/lexer/lexer.dart';
+import 'package:todo_as_issue/lexer/tokens.dart';
 
 void main(List<String> args) async {
   File file = File('examples/todo.txt');
   String content = await file.readAsString();
   Lexer lexer = Lexer(content);
+  List<Token> tokens = lexer.tokenize();
+
+  for (Token token in tokens) {
+    print(token);
+  }
 }
