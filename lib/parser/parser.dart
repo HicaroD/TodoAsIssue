4747a95dd7530b2fe4dc7b9ec2e41059a47d3083
HicaroD <hdanrlley1@gmail.com>
2022-11-07 10:26:46 -0300

    parser: implementando parser
    Basicamente o parser servirá para converter a lista de tokens em uma
    lista de Todo (da classe 'Todo')

--- lib/parser/parser.dart
+++ lib/parser/parser.dart
@@ -1,19 +1,20 @@
 import 'package:todo_as_issue/lexer/tokens.dart';
+import 'package:todo_as_issue/parser/error_reporter.dart';
 import 'package:todo_as_issue/parser/todo.dart';
 
 class TokenIterator implements Iterator {
   final List<Token> tokens;
-  int currentPosition = 0;
+  int cursor = 0;
 
   TokenIterator(this.tokens);
 
   @override
-  get current => tokens[currentPosition];
+  get current => tokens[cursor];
 
   @override
   bool moveNext() {
     if (hasNext()) {
-      currentPosition++;
+      cursor++;
       return true;
     } else {
       return false;
@@ -21,23 +22,77 @@ class TokenIterator implements Iterator {
   }
 
   bool hasNext() {
-    return currentPosition < tokens.length;
+    return cursor < tokens.length;
   }
 }
 
 class Parser {
   final List<Token> tokens;
+  ErrorReporter errorReporter = ErrorReporter();
 
   Parser(this.tokens);
 
   List<Todo> parse() {
     List<Todo> todos = [];
-    TokenIterator tokenIterator = TokenIterator(tokens);
+    TokenIterator iterator = TokenIterator(tokens);
 
-    while (tokenIterator.hasNext()) {
-      Token currentToken = tokenIterator.current;
-      print(currentToken);
-      tokenIterator.moveNext();
+    while (iterator.hasNext()) {
+      if (iterator.current.kind != TokenKind.hashSymbol) {
+        errorReporter.reportError(iterator.current);
+      }
+      iterator.moveNext();
+
+      if (iterator.current.kind != TokenKind.openingParenthesis) {
+        errorReporter.reportError(iterator.current);
+      }
+      iterator.moveNext();
+
+      if (iterator.current.kind != TokenKind.number) {
+        errorReporter.reportError(iterator.current);
+      }
+      int todoId = int.parse(iterator.current.lexeme);
+      iterator.moveNext();
+
+      if (iterator.current.kind != TokenKind.closingParenthesis) {
+        errorReporter.reportError(iterator.current);
+      }
+      iterator.moveNext();
+
+      if (iterator.current.kind != TokenKind.openingSquareBracket) {
+        errorReporter.reportError(iterator.current);
+      }
+      iterator.moveNext();
+
+      bool isClosed = false;
+      if (iterator.current.kind == TokenKind.tilde) {
+        isClosed = true;
+        iterator.moveNext();
+      }
+
+      if (iterator.current.kind != TokenKind.closingSquareBracket) {
+        errorReporter.reportError(iterator.current);
+      }
+      iterator.moveNext();
+
+      if (iterator.current.kind != TokenKind.colon) {
+        errorReporter.reportError(iterator.current);
+      }
+      iterator.moveNext();
+
+      if (iterator.current.kind != TokenKind.issueName) {
+        errorReporter.reportError(iterator.current);
+      }
+      String issueTitle = iterator.current.lexeme;
+      iterator.moveNext();
+
+      if (iterator.current.kind != TokenKind.semicolon) {
+        errorReporter.reportError(iterator.current);
+      }
+
+      if (iterator.hasNext()) iterator.moveNext();
+
+      Todo todo = Todo(id: todoId, isClosed: isClosed, title: issueTitle);
+      todos.add(todo);
     }
     return todos;
   }
