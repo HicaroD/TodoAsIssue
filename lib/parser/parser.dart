fef632e69eddb22b94ad1270d8bff52b943fe969
HicaroD <hdanrlley1@gmail.com>
2022-11-07 09:01:51 -0300

    parser: implementando `Iterator` pattern para o parser

--- lib/parser/parser.dart
+++ lib/parser/parser.dart
@@ -1,6 +1,30 @@
 import 'package:todo_as_issue/lexer/tokens.dart';
 import 'package:todo_as_issue/parser/todo.dart';
 
+class TokenIterator implements Iterator {
+  final List<Token> tokens;
+  int currentPosition = 0;
+
+  TokenIterator(this.tokens);
+
+  @override
+  get current => tokens[currentPosition];
+
+  @override
+  bool moveNext() {
+    if (hasNext()) {
+      currentPosition++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool hasNext() {
+    return currentPosition < tokens.length;
+  }
+}
+
 class Parser {
   final List<Token> tokens;
 
@@ -8,6 +32,13 @@ class Parser {
 
   List<Todo> parse() {
     List<Todo> todos = [];
+    TokenIterator tokenIterator = TokenIterator(tokens);
+
+    while (tokenIterator.hasNext()) {
+      Token currentToken = tokenIterator.current;
+      print(currentToken);
+      tokenIterator.moveNext();
+    }
     return todos;
   }
 }
