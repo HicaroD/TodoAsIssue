06ae91c544e18f690f6aeb1c796470a301251313
HicaroD <hdanrlley1@gmail.com>
2022-11-10 09:52:14 -0300

    main: construindo métodos ajudantes para ler arquivos
    Esses métodos ajudantes estão lendo o arquivo de TODO e o arquivo de
    configuração no formato JSON contendo `owner`, `repo_name` e
    `private_token`

--- bin/todo_as_issue.dart
+++ bin/todo_as_issue.dart
@@ -1,16 +1,43 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:todo_as_issue/api/github.dart';
 import 'package:todo_as_issue/lexer/lexer.dart';
 import 'package:todo_as_issue/lexer/tokens.dart';
 import 'package:todo_as_issue/parser/parser.dart';
 import 'package:todo_as_issue/parser/todo.dart';
 
-void main(List<String> args) async {
-  File file = File('examples/todo.txt');
-  String content = await file.readAsString();
+Future<Map<String, dynamic>> getConfigFile() async {
+  String path = 'todo.json';
+  File jsonFile = File(path);
 
-  Lexer lexer = Lexer(content);
+  if (!await jsonFile.exists()) {
+    print(
+        "ERROR: Configuration file 'todo.json' not found on project root folder");
+    exit(1);
+  }
+  Map<String, dynamic> jsonDecoded = jsonDecode(await jsonFile.readAsString());
+  return jsonDecoded;
+}
+
+Future<String> getTodoFile(String filePath) async {
+  File todoFile = File(filePath);
+  if (!await todoFile.exists()) {
+    print("ERROR: 'todo.txt' not found");
+    exit(1);
+  }
+  String content = await todoFile.readAsString();
+  return content;
+}
+
+void main(List<String> args) async {
+  String todoFile = await getTodoFile("examples/todo.txt");
+  Map<String, dynamic> settingsAsJson = await getConfigFile();
+
+  Lexer lexer = Lexer(todoFile);
   List<Token> tokens = lexer.tokenize();
 
   Parser parser = Parser(tokens);
   List<Todo> todos = parser.parse();
+
+  GitHub github = GitHub.instance;
 }
