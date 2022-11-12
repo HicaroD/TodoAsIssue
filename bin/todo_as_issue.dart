0e0c20bf2a7c49c3708dd378b6e2607df20378d4
HicaroD <hdanrlley1@gmail.com>
2022-11-12 07:40:23 -0300

    feature/cli: implementing cli for getting interface

--- bin/todo_as_issue.dart
+++ bin/todo_as_issue.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:args/args.dart';
 import 'package:todo_as_issue/api/api.dart';
 import 'package:todo_as_issue/api/github.dart';
+import 'package:todo_as_issue/api/gitlab.dart';
+import 'package:todo_as_issue/api/opensource_platform.dart';
 import 'package:todo_as_issue/lexer/lexer.dart';
 import 'package:todo_as_issue/lexer/tokens.dart';
 import 'package:todo_as_issue/parser/parser.dart';
@@ -15,8 +18,12 @@ class TodoAsIssue {
   late API api;
   final Configuration configuration;
   final String todoFile;
+  final OpenSourcePlatform openSourcePlatform;
 
-  TodoAsIssue({required this.todoFile, required this.configuration});
+  TodoAsIssue(
+      {required this.todoFile,
+      required this.configuration,
+      required this.openSourcePlatform});
 
   void run() async {
     lexer = Lexer(todoFile);
@@ -24,20 +31,35 @@ class TodoAsIssue {
     parser = Parser(tokens);
     List<Todo> todos = parser.parse();
 
-    // TODO: implementar interface para fazer usuário decidir entre GitHub ou GitLab
-    GitHub github = GitHub();
-    API api = API(github);
+    API api = API(openSourcePlatform);
 
     api.createIssues(todos, configuration);
   }
+}
+
+OpenSourcePlatform getPlatformFromCommandLine(List<String> args) {
+  ArgParser parser = ArgParser();
+  parser.addOption("platform", abbr: "p", defaultsTo: "github");
+  ArgResults results = parser.parse(args);
+
+  OpenSourcePlatform openSourcePlatform = GitHub();
+
+  if (results["platform"] == "Gitlab") {
+    openSourcePlatform = GitLab();
+  }
+
+  return openSourcePlatform;
 }
 
 void main(List<String> args) async {
   String todoFile = await Reader.getTodoFile("examples/todo.txt");
   Map<String, dynamic> configAsJson = await Reader.getConfigFile();
   Configuration configuration = Configuration.fromJson(configAsJson);
+  OpenSourcePlatform openSourcePlatform = getPlatformFromCommandLine(args);
 
-  TodoAsIssue todoAsIssue =
-      TodoAsIssue(todoFile: todoFile, configuration: configuration);
+  TodoAsIssue todoAsIssue = TodoAsIssue(
+      todoFile: todoFile,
+      configuration: configuration,
+      openSourcePlatform: openSourcePlatform);
   todoAsIssue.run();
 }
