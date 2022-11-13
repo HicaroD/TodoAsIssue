359a28e939b0c188930b9d4e7027e1bd2ceb8a52
HicaroD <hdanrlley1@gmail.com>
2022-11-13 10:44:46 -0300

    api: implementing flag for checking if todo was posted or not

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:todo_as_issue/api/opensource_platform.dart';
+import 'package:todo_as_issue/core/http_client/http_client_interface.dart';
 
 import '../parser/todo.dart';
 import '../utils/configuration.dart';
@@ -16,7 +17,18 @@ class API {
 
   void createIssues(List<Todo> todos, Configuration configuration) async {
     for (Todo todo in todos) {
-      _openSourcePlatform.createIssue(todo, configuration);
+      if (!todo.wasPosted) {
+        HttpResponse response =
+            await _openSourcePlatform.createIssue(todo, configuration);
+
+        if (response.statusCode != 201) {
+          print("Error: Can't create issue");
+          print(response.statusCode);
+          print(response.body);
+          exit(1);
+        }
+        print("🎉 Issue was created successfully 🎉");
+      }
     }
     // This is useful for avoiding problems with GitHub's rate limit policies
     sleep(Duration(seconds: 2));
